503b5255b6ef0154b02c17fc583fc21a1896d25c
Ishmam90 <ishmamkabir90@gmail.com>
2026-01-26 21:22:01 +0600

    hotel updated

---
+++ hotel.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+class Hotel{
+    private:
+        int hotelId;
+        string name;
+        string location;
+        static vector<int> id;
+
+    public:
+        Hotel(string name, string location){
+            srand(time(NULL));
+            int h_id=rand()%(9999-1000+1)+1000;
+
+            while(1){
+                auto it=find(id.begin(), id.end(), h_id);
+
+                if(it!=id.end()){
+                    h_id=rand()%(9999-1000+1)+1000;
+                }
+
+                else{
+                    hotelId=h_id;
+                    id.push_back(h_id);
+                    break;
+                }
+            }
+
+            this->name=name;
+            this->location=location;
+        }
+
+        int get_id()const{
+            return hotelId;
+        }
+
+        string get_name()const{
+            return name;
+        }
+
+        string get_location()const{
+            return location;
+        }        
+};
