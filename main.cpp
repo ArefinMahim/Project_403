d608538bec92cf41a1cf7b44498e3173632bc3c9
ArefinMahim <arefinmahim14@gmail.com>
2026-01-28 02:29:46 +0600

    Main.cpp to check the Person class is working

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+    Person p1(1, "Arefin", "Mahim", "01319998958", "arefinmahim14@gmail.com", "Aftabnagar, Dhaka");
+    p1.DisplayInfo();
+    return 0;
+}
