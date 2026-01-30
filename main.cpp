a3bd0545922f1c5eb43af96be8dcc43bcc0d0739
ArefinMahim <arefinmahim14@gmail.com>
2026-01-30 19:30:30 +0600

    Main.cpp is updated to check the Guest class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-#include "Person.h"
+#include "Person.cpp"
+#include "Guest.cpp"
 
 using namespace std;
 
@@ -7,5 +8,8 @@ int main()
 {
     Person p1(1, "Arefin", "Mahim", "01319998958", "arefinmahim14@gmail.com", "Aftabnagar, Dhaka");
     p1.DisplayInfo();
+
+    Guest g1(2, "Fabian", "Mahdi", "01670165797", " fabianmahdi@gmail.com", "Elephant Road, Dhaka", "12345", 302, "15-01-2026", "20-01-2026");
+    g1.DisplayInfo();
     return 0;
 }
