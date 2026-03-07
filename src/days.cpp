eb7350d4e801d84033a1e599ee8442daa75e688c
Fabikuun <fabianmahdi123@gmail.com>
2026-03-07 18:50:01 +0600

    Updated to use run time error and error handling, also implemented operator overloading ++ for days

--- src/days.cpp
+++ src/days.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include"utility.hpp"
 #include"days.hpp"
 using namespace std;
 
@@ -8,8 +8,7 @@ Days::Days(int d, int m, int y){
 }
 
 bool Days::isLeapYear(int y) const{
-    if((y%4==0 && y%100!=0) || (y%400==0)) return true;
-    return false;
+    return ((y%4==0 && y%100!=0) || (y%400==0));
 }
 int Days::monthDays(int m, int y) const{
     static int month[12]={31,28,31,30,31,30,31,31,30,31,30,31};
@@ -17,10 +16,7 @@ int Days::monthDays(int m, int y) const{
     return month[m-1];
 }
 void Days::setDate(int d, int m, int y){
-    if(d>monthDays(m,y) || m<1 || m>12 || d<1 || y<1){
-        cout<<"Invalid date."<<endl;
-        return;
-    }
+    if(m<1 || m>12 || y<2000 || d<1 || d>monthDays(m,y)) throw runtime_error("Invalid date");
     date=d;
     month=m;
     year=y;
@@ -73,8 +69,26 @@ int Days::noOfDays(const Days& toDate) const{
     totalDays+=endDate.date;
     return totalDays;
 }
-std::ostream& operator<<(std::ostream &os, const Days &d){
+ostream& operator<<(ostream &os, const Days &d){
     os<<d.date<<"/"<<d.month<<"/"<<d.year;
     return os;
+}
+Days Days::operator++(int){
+    Days temp=*this;
+    addDays(1);
+    return temp;
+}
+Days Days::operator--(int){
+    Days temp=*this;
+    date--;
+    if(date<1){
+        month--;
+        if(month<1){
+            month=12;
+            year--;
+        }
+        date=monthDays(month, year);
+    }
+    return temp;
 }
 Days::~Days(){}
