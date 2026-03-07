a2cfd540f0d09c5138f5acc05f9eba3c9ca9e6cf
Ishmam90 <ishmamkabir90@email.com>
2026-03-07 20:46:43 +0600

    hotel changes

--- src/hotel.cpp
+++ src/hotel.cpp
@@ -1,58 +1,56 @@
 #include "hotel.hpp"
 
-//Hotel Class Functions
+// Hotel Class Functions
 
-        Hotel::Hotel(string name, string location){
-            srand(time(NULL));
-            int h_id=rand()%(9999-1000+1)+1000;
+Hotel::Hotel(string name, string location) {
+    srand(time(NULL));
+    int h_id = rand() % (9999 - 1000 + 1) + 1000;
 
-            while(1){
-                auto it=find(id.begin(), id.end(), h_id);
+    while (1) {
+        auto it = find(id.begin(), id.end(), h_id);
 
-                if(it!=id.end()){
-                    h_id=rand()%(9999-1000+1)+1000;
-                }
-
-                else{
-                    hotel_Id=h_id;
-                    id.push_back(h_id);
-                    break;
-                }
-            }
-
-            this->name=name;
-            this->location=location;
+        if (it != id.end()) {
+            h_id = rand() % (9999 - 1000 + 1) + 1000;
         }
 
-        int Hotel::get_id()const{
-            return hotel_Id;
+        else {
+            hotel_Id = h_id;
+            id.push_back(h_id);
+            break;
         }
+    }
 
-        string Hotel::get_name()const{
-            return name;
-        }
+    this->name = name;
+    this->location = location;
+}
 
-        string Hotel::get_location()const{
-            return location;
-        }
+int Hotel::get_id() const { return hotel_Id; }
+string Hotel::get_name() const { return name; }
+string Hotel::get_location() const { return location; }
 
-        void Hotel::set_name(string name){
-            this->name=name;
-        }
+void Hotel::set_name(string name) { this->name = name; }
+void Hotel::set_location(string location) { this->location = location; }
+void Hotel::add_room(Room &room) {
+    rooms.push_back(room);      //rooms needed
+}
 
-        void Hotel::set_location(string location){
-            this->location=location;
-        }
+void Hotel::print_details() const {
+    cout << "Hotel ID: " << hotel_Id << endl;
+    cout << "Hotel Name: " << name << endl;
+    cout << "Hotel Location: " << location << endl;
+    cout << "Number of Rooms: " << rooms.size() << endl;
+}
 
-        void Hotel::add_room(Room& room){
-            //rooms.push_back(room);
-        }
+const vector<Room*>& Hotel::get_rooms() const{
+    return rooms;
+}
 
-        void Hotel::print_details()const{
-            cout<<"Hotel ID: "<<hotel_Id<<endl;
-            cout<<"Hotel Name: "<<name<<endl;
-            cout<<"Hotel Location: "<<location<<endl;
-            cout<<"Number of Rooms: "<<rooms.size()<<endl;
-        }
+Room* Hotel :: find_room(const string& roomID){
+    for(auto* r:rooms){
+        if(r->get_room_ID()==roomID)    return r;
+    }
 
-        vector<int> Hotel::id = {0};
+    return NULL;
+}
+
+vector<int> Hotel::id = {0};
