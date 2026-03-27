af71e3cd468c7e175f6733993f042a1c2dd5334e
SaladParadox <jarofsalad43@gmail.com>
2026-03-28 00:14:19 +0600

    Created main file

--- main.cpp
+++ main.cpp
@@ -1,30 +1,14 @@
-#include<iostream>
-#include "Person.cpp"
-#include "Guest.cpp"
-
+#include "header/hotelsystem.hpp"
+#include "header/ui.hpp"
+ 
 using namespace std;
-
-int main()
-{
-    Person p1(1, "Arefin", "Mahim", "01319998958", "arefinmahim14@gmail.com", "Aftabnagar, Dhaka");
-    p1.DisplayInfo();
-
-    Guest g1(2, "Fabian", "Mahdi", "01670165797", " fabianmahdi@gmail.com", "Elephant Road, Dhaka", "12345", 302, "15-01-2026", "20-01-2026");
-    g1.DisplayInfo();
-    return 0;
-}
-
-#include "header/person.hpp"
-#include "header/guest.hpp"
-
-using namespace std;
-
-int main()
-{
-    Person p1(1, "Arefin", "Mahim", "01319998958", "arefinmahim14@gmail.com", "Aftabnagar, Dhaka");
-    p1.DisplayInfo();
-
-    Guest g1(2, "Fabian", "Mahdi", "01670165797", " fabianmahdi@gmail.com", "Elephant Road, Dhaka", "12345", 302, "15-01-2026", "20-01-2026");
-    g1.DisplayInfo();
+ 
+int main() {
+    srand(time(NULL));   
+ 
+    HotelSystem sys;     
+    UI ui(sys);
+    ui.run();           
+ 
     return 0;
 }
